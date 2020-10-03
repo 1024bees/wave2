83fc33252866b83f4e0d08fbfd61024729b002a0
James Connolly <jimmy@1024bees.com>
2020-10-02 21:37:09 -0400

    hier map doesnt compile; need to have some recurse helper like used for idmap

--- wavedb/src/hier_map.rs
+++ wavedb/src/hier_map.rs
@@ -3,27 +3,46 @@ use vcd::{IdCode,ScopeItem};
 use std::cell::Cell;
 use crate::errors::Waverr;
 
-pub struct HierMap<'a>(Vec<ModuleItem<'a>>);
-
-type ModRef<'a> = Option<&'a mut ModuleItem<'a>>;
-
-impl<'a> From<&vcd::Header> for HierMap<'a> {
-    fn from(header: &vcd::Header) -> HierMap<'a> {
+pub struct HierMap(Vec<ModuleItem>);
 
 
-        let mut HierMapVec = Vec::new();
-        let mut parentmod : ModRef<'a> = None;
-        let mut livemod_ref : ModRef<'a> = None;
+impl From<&vcd::Header> for HierMap {
+    fn from(header: &vcd::Header) -> HierMap {
+
+
+        let mut HierMapVec : Vec<ModuleItem> = Vec::new();
+        let mut parentmod : Option<usize>  = None;
+        let mut livemod_ref : usize = 0;
+
+        fn recurse_parse(
+            map: &mut Vec<ModuleItem>,
+            scope: &vcd::Scope,
+            parent_mod : Option<usize>
+        ) {
+
+        }
+
+
+
+
+
+
+
+
+
         for item in header.items.iter() {
             match item {
                 ScopeItem::Var(variable) => {
-                    livemod_ref.unwrap().add_sig(SignalItem::from(variable));
+                    HierMapVec[livemod_ref].add_sig(SignalItem::from(variable));
                 }
                 ScopeItem::Scope(scope) => {
-                    parentmod = livemod_ref;
+                    if let Some(parent_idx)  = parentmod {
+                        HierMapVec[parent_idx].add_
+                    }
+                    parentmod = Some(livemod_ref);
                     HierMapVec.push(ModuleItem::new(scope.identifier.clone(),parentmod));
-                    livemod_ref = HierMapVec.last_mut();
-
+                    livemod_ref = HierMapVec.len() - 1;
+                    
                 }
             }
         }
@@ -33,11 +52,11 @@ impl<'a> From<&vcd::Header> for HierMap<'a> {
     }
 }
 #[derive(Default)]
-pub struct ModuleItem<'a> {
+pub struct ModuleItem {
     name : String,
-    submodules : Vec<&'a ModuleItem<'a>>,
+    submodules : Vec<usize>,
     signals : Vec<SignalItem>,
-    parent : Option<&'a ModuleItem<'a>>
+    parent : Option<usize>
     
 }
 
@@ -48,8 +67,8 @@ impl From<&vcd::Var> for SignalItem {
     }
 }
 
-impl<'a> ModuleItem<'a> {
-    fn new(name: String, parent : ModRef<'a> ) -> Self {
+impl ModuleItem {
+    fn new(name: String, parent : Option<usize> ) -> Self {
         ModuleItem {
             name,
             parent,
